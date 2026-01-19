6041a036e1f0afcbdfc6d31bffef976a8e3a9d4e
Sanjaya Pokharel <psanzay1@gmailcom>
2026-01-19 18:14:14 +0545

    timeout increased

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -1,8 +1,11 @@
 import {test, expect} from "@playwright/test";
 
 test('title of the page', async ({page})=>{
-   await test.waitForTimeout(15000);
-   await page.goto('http://www.automationpractice.pl/index.php');
+    await page.goto('http://www.automationpractice.pl/index.php');
+      //await page.waitForTimeout(15000);
+
+    test.setTimeout(150000);
+  
     let title:string = await page.title();
     console.log("Title is: ", title);
    await expect(page).toHaveTitle("My Shop");
